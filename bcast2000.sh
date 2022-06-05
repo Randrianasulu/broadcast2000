9e1aa59ff234ee527c068ad7e02a6ce79c02a101
Andrew Randrianasulu <randrianasulu@gmail.com>
2022-06-05 11:54:05 +0300

    Try to autodetect oss/v4l emulation in bcast2000.sh script

--- bcast2000.sh
+++ bcast2000.sh
@@ -2,6 +2,18 @@
 
 BCASTDIR=/usr/local/bcast
 
+if [ -x /usr/bin/aoss ]; then
+OSS_EMU=aoss
+fi
+if [ -x /usr/bin/padsp ]; then
+OSS_EMU=padsp
+fi
+
+V4L_EMU=`find /usr -name v4l1compat.so`
+
+#echo $OSS_EMU
+#echo $V4L_EMU
+
 export LD_LIBRARY_PATH=$BCASTDIR:$LD_LIBRARY_PATH
 export PATH=$BCASTDIR:$PATH
-exec aoss $BCASTDIR/bcast2000 $@
+LD_PRELOAD=$V4L_EMU $OSS_EMU $BCASTDIR/bcast2000 $@
